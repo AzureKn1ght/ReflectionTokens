7d9e8c3b7843c01dcaa2763c498786b4feae3426
AzureKn1ght <philsonnah@msn.com>
2022-03-08 16:59:52 +0800

    Added Code

---
+++ SkeletonCode.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.12;
+
+
+/*
+
+SKELETON CODE BASED ON SAFEMOON
+FOR LEARNING PURPOSES ONLY
+
+*/
+
+
+
+
+//interface IERC20
+
+//library SafeMath
+
+//abstract contract Context
+
+//library Address
+
+//contract Ownable is Context
+
+//interface IUniswapV2Factory
+
+//interface IUniswapV2Pair
+
+//interface IUniswapV2Router01
+
+//interface IUniswapV2Router02 is IUniswapV2Router01
+
+
+//contract SafeMoon is Context, IERC20, Ownable
